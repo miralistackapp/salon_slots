07696e1ead698cf4afd14456546a52d2dea8e676
Mirali <miralistackapp@gmail.com>
2021-07-07 10:28:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# salon_slots
+ salon_slots project
---
+++ android/app/src/main/kotlin/com/example/salon_slots/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.salon_slots
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
